9886bb5ee5badee7d344c2a61a5ca1452f03699a
akcm-dungpt <phamtiendung@luvina.net>
2017-04-13 13:41:13 +0700

    Done

--- wp-content/themes/chungcuxanh/archive.php
+++ wp-content/themes/chungcuxanh/archive.php
@@ -14,45 +14,47 @@ get_header(); ?>
     <div id="primary" class="content-area">
         <!-- Du an BDS -->
         <div class="row">
-            <header class="page-header">
-            <?php
-                sociallyviral_archive_title( '<h1 class="page-title">', '</h1>' );
-                sociallyviral_archive_description( '<div class="taxonomy-description">', '</div>' );
-            ?>
-            </header><!-- .page-header -->
-            <?php
-            $id_Category = the_category_ID($echo=false);
-            if ($id_Category) {
-            $posts_array = get_posts( array(category=>$id_Category, numberposts=>'100') );
-            foreach ( $posts_array as $post ) : setup_postdata( $post ); ?>
-                <div id="box1" class="col">
-                    <img src="<?php echo sociallyviral_get_thumbnail_url('sociallyviral_featured'); ?>" alt="<?php the_title(); ?>" />
-                    <h2>
-						<a href="<?php the_permalink(); ?>">
-							<?php the_title(); ?> <br />
-							<?php echo get_post_meta($post->ID, 'tp_country', TRUE);?>
-						</a>
-					</h2>
+            <?php if ( have_posts() ) : ?>
+                <header class="page-header">
+                <?php
+                    sociallyviral_archive_title( '<h1 class="page-title">', '</h1>' );
+                    sociallyviral_archive_description( '<div class="taxonomy-description">', '</div>' );
+                ?>
+                </header><!-- .page-header -->
+                <?php
+                $id_Category = the_category_ID($echo=false);
+                if ($id_Category) {
+                $posts_array = get_posts( array(category=>$id_Category, numberposts=>'100') );
+                $total_page = count($posts_array);
+                while ( have_posts() ) : the_post();
+                ?>
+                    <div id="box1" class="col">
+                        <img src="<?php echo sociallyviral_get_thumbnail_url('sociallyviral_featured'); ?>" alt="<?php the_title(); ?>" />
+                        <h2>
+    						<a href="<?php the_permalink(); ?>">
+    							<?php the_title(); ?> <br />
+    							<div>
+    							    <span class="post_don_gia">Giá bán: </span>
+    							    <span><?php echo get_post_meta($post->ID, 'don_gia', TRUE);?></span>
+    							</div>
+    							<div>
+    							    <span class="post_don_gia">Địa chỉ: </span>
+    							    <span><?php echo get_post_meta($post->ID, 'dia_chi', TRUE);?></span>
+    							</div>
+    						</a>
+    					</h2>
+                    </div>
+                <?php endwhile; ?>
+                <div class="phan_trang">
+                    <?php forest_pagination(); ?>
                 </div>
-            <?php endforeach;
-            if(paginate_links()!='') {?>
-            	<div class="quatrang">
-            		<?php
-            		global $wp_query;
-            		$big = 999999999;
-            		echo paginate_links( array(
-            			'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
-            			'format' => '?paged=%#%',
-            			'prev_text'    => __('« Mới hơn'),
-            			'next_text'    => __('Tiếp theo »'),
-            			'current' => max( 1, get_query_var('paged') ),
-            			'total' => $wp_query->max_num_pages
-            			) );
-            	    ?>
-            	</div>
-            <?php }
-            }?>
+                <?php }?>
+            <?php else : ?>
+                <?php get_template_part( 'content', 'none' ); ?>
+            <?php endif; ?>
+            
         </div>
+        
     </div><!-- #primary -->
     <?php get_sidebar();?>
 </div> <!-- .contents-top -->
--- wp-content/themes/chungcuxanh/functions.php
+++ wp-content/themes/chungcuxanh/functions.php
@@ -371,3 +371,27 @@ function sociallyviral_get_thumbnail_url( $size = 'full' ) {
     else
         return '';
 }
+
+/*
+ * Pagination Function. Implements core paginate_links function.
+ */
+function forest_pagination() {
+	global $wp_query;
+	$big = 12345678;
+	$page_format = paginate_links( array(
+	    'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+	    'format' => '?paged=%#%',
+	    'current' => max( 1, get_query_var('paged') ),
+	    'total' => $wp_query->max_num_pages,
+	    'type'  => 'array'
+	) );
+	if( is_array($page_format) ) {
+	            $paged = ( get_query_var('paged') == 0 ) ? 1 : get_query_var('paged');
+	            echo '<div class="pagination"><div><ul>';
+	            //echo '<li><span>'. $paged . ' of ' . $wp_query->max_num_pages .'</span></li>';
+	            foreach ( $page_format as $page ) {
+	                    echo "<li>$page</li>";
+	            }
+	           echo '</ul></div></div>';
+	 }
+}
